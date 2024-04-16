1abb84b6e79ed7a53efea3fd09605d98f3644646
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-04-17 09:57:24 +1000

    add hash.py and fix filenames

--- prime.py
+++ prime.py
@@ -1,32 +1,58 @@
-import random
+try:
+    from ctypes import windll
+    import tkinter as tk
+    from tkinter import filedialog
+except ImportError:
+    import Tkinter as tk
+    import tkFileDialog as filedialog
+
+windll.shcore.SetProcessDpiAwareness(1)  # fixes the blurry file dialog
+
+root = tk.Tk()
+root.withdraw()
+root.attributes("-topmost", True)  # Opened window will be above all other windows even if clicked off
 
 prime = []
 nonprime = []
 
-for i in range(10):
-    num = random.randint(i, 1000)
-    if num > 1:
-        for x in range(2, num):
-            if (num % x) == 0:
-                nonprime.append(str(num) + " (" + str(i) + u"\u00D7" + str(num // x) + ")")
-                break
-        else:
-            prime.append(num)
+def main():
+    print("Open the Number List")
+    filename = filedialog.askopenfilenames(filetypes=[("Text", ".txt")])
+
+    if not len(filename) >= 1:  # they didnt select a file
+        print("Invalid File \n")
+        main()
+
+    with open("".join(filename), "r") as f:
+        startlist = f.readlines()
+        startlist = [x.strip() for x in startlist]  # removes spaces
+
+    for i in startlist:
+        try:
+            if int(i) > 1:
+                for x in range(2, int(i)):
+                    if (int(i) % x) == 0:
+                        nonprime.append(i + " (" + str(x) + u"\u00D7" + str(int(i) // x) + ")")
+                        break
+                else:
+                    prime.append(i)
+            else:  # num is 0 or  1
+                nonprime.append(i)
+        except ValueError:  # if there is a letter in document
+            pass
+
+    sorted(prime)
+    sorted(nonprime)
+
+    if len(prime) == 0:
+        print("No Prime Numbers")
     else:
-        nonprime.append(num)
+        print("Prime Numbers:", ", ".join(prime))
 
-prime.sort()
-nonprime.sort()
+    if len(nonprime) == 0:
+        print("No Non Prime Numbers")
+    else:
+        print("Non Prime Numbers:", ", ".join(nonprime))
 
-prime_new = [str(a) for a in prime]
-nonprime_new = [str(a) for a in nonprime]
-
-if len(prime) == 0:
-    print("No Prime Numbers")
-else:
-    print("Prime Numbers:", ", ". join(prime_new))
-
-if len(nonprime) == 0:
-    print("No Non Prime Numbers")
-else:
-    print("Non Prime Numbers:", ", ". join(nonprime_new))
+if __name__ == "__main__":
+    main()
