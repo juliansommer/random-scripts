122dd1546f664a1f16f90d18dca4c8d4fbe801f9
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2025-06-09 11:44:30 +1000

    add recursive binary search

--- binary_search.py
+++ binary_search.py
@@ -8,9 +8,11 @@ def binary_search(arr: List[int], target: int) -> int:
     while low <= high:
         mid = low + ((high - low) // 2)
 
+        # if number too high, search left half
         if arr[mid] > target:
             high = mid - 1
 
+        # if number too low, search right half
         elif arr[mid] < target:
             low = mid + 1
 
@@ -20,13 +22,39 @@ def binary_search(arr: List[int], target: int) -> int:
     return -1
 
 
+def recursive_binary_search(arr: List[int], target: int, low: int, high: int) -> int:
+    if low > high:
+        return -1
+
+    mid = low + ((high - low) // 2)
+
+    # if number too high, search left half
+    if arr[mid] > target:
+        return recursive_binary_search(arr, target, low, mid - 1)
+
+    # if number too low, search right half
+    elif arr[mid] < target:
+        return recursive_binary_search(arr, target, mid + 1, high)
+
+    else:
+        return mid
+
+
 def main() -> None:
     arr = [1, 3, 5, 8, 13, 21, 55, 89, 144, 233]
     target = 8
     result = binary_search(arr, target)
+    result2 = recursive_binary_search(arr, target, 0, len(arr) - 1)
 
+    print("Iterative")
     if result != -1:
         print(f"Element is present at index {result}")
+    else:
+        print("Element is not present in list")
+
+    print("Recursive")
+    if result2 != -1:
+        print(f"Element is present at index {result2}")
     else:
         print("Element is not present in list")
 
